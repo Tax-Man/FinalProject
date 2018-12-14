ba0d5a22d4fc90a12d0e1ceb9ec17aa2e573da48
Lance Trudel <lancetru@mymts.net>
2018-12-14 15:33:59 -0600

    twest

---
+++ FinalProject40S/FinalProject40S/Aircraft.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinalProject40S
+{
+    public abstract class Aircraft
+    {
+        public PointF Target
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+    }
+}
---
+++ FinalProject40S/FinalProject40S/Airport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinalProject40S
+{
+    public class Airport
+    {
+        public Aircraft Aircraft
+        {
+            get => default(Aircraft);
+            set
+            {
+            }
+        }
+
+        public int Capacity
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+    }
+}
